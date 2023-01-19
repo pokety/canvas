73bef3b89fc4fc4b470a1e41aaa2e5faf7a33b64
Claudio Amilton <pokety@gmail.com>
2023-01-18 23:43:12 -0300

    ...1

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express=require("express");
 const app=express();
 const server=require("http").createServer(app);
 const path=require('path');
+const { emit } = require("process");
 const io=require("socket.io")(server);
 require("dotenv").config()
-// app.use('public',express.static('/public'))
+app.use('public',express.static('/public'))
 
 app.get("/",(req,res)=>{
     
-    res.sendFile(path.join(__dirname + "/index.html"))
+    res.sendFile(path.join(__dirname + "/public/index.html"))
 })
 
 function getRandomInt(min, max) {
@@ -20,16 +21,28 @@ function getRandom10(min, max) {
 
 
 var pos_player={}
-var pos_fruit={f_x:getRandom10(-10,500),f_y:getRandom10(-10,500)}
-io.on("connection",(socket)=>{
 
-    
+io.on("connection",(socket)=>{
     console.log('connectado: '+socket.id)
-    socket.on("eat",(eat)=>{
-        console.log(eat)
-    })
-    socket.on("enviar",(msg)=>{
-        Object.assign(pos_player,{[socket.id]:msg})
+   
+    socket.on("enviar",(locate)=>{
+        
+        var pos_fruit={f_x:50,f_y:50}
+        Object.assign(pos_player,{[socket.id]:locate})
+
+
+        let id=Object.values(pos_player)
+            let a;
+
+        for(a=0;a<id.length;a++){                 
+            if(id[a].pos_x == pos_fruit.f_x && id[a].pos_y == pos_fruit.f_y){
+                console.log(pos_fruit)
+                io.emit('eat',{f_x:getRandom10(0,490),f_y:getRandom10(0,490)})
+            }
+            
+        }
+        
+        //socket.broadcast.emit('all',pos_player,pos_fruit)
         io.emit('all',pos_player,pos_fruit);
     })
     socket.on('disconnect',()=>{
